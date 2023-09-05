8c9de3986484115ca159197aaf54bdad2b7f5d07
Carlos-Simim <42616810+Carlos-Simim@users.noreply.github.com>
2023-09-04 23:24:34 -0300

    Update /d
    Adição de validação da quantidade mínima de lados para um dado.

--- dice.py
+++ dice.py
@@ -9,6 +9,12 @@ class dice(commands.Cog):
 
     @commands.slash_command(name="d", description="Rola um dado de quantos lados você quiser.")
     async def d(self, ctx, lados: int):
+        if lados <= 1:
+            await ctx.send(f'{ctx.author.mention}, um dado não pode ter lados negativos, eu heim.')
+            return
+        if lados == 2:
+            await ctx.send(f'{ctx.author.mention}, um dado de 2 lados é uma moeda, use o /coinflip. :rolling_eyes:')
+            return
         resultado = random.randint(1, lados)
         await ctx.send(f'{ctx.author.mention} rolou um d{lados}! Resultado: ``{resultado}``')
 
